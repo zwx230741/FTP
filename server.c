1f3447115b248a7b0462bae5d0c9e7155d66cb00
thomas <1798211707@qq.com>
2019-03-12 10:21:51 +0800

    格式化代码

--- server.c
+++ server.c
@@ -1,72 +1,88 @@
 #include "common.h"
-/** 
+/**
  * Sets up server and handles incoming connections
  * @param port Server port
  */
 void server(int port)
 {
-  int sock = create_socket(port);
-  struct sockaddr_in client_address;
-  int len = sizeof(client_address);
-  int connection, pid, bytes_read;
+   int sock = create_socket(port);
+   struct sockaddr_in client_address;
+   int len = sizeof(client_address);
+   int connection, pid, bytes_read;
 
-  while(1){
-    connection = accept(sock, (struct sockaddr*) &client_address,&len);
-    char buffer[BSIZE];
-    Command *cmd = malloc(sizeof(Command));
-    State *state = malloc(sizeof(State));
-    pid = fork();
-    
-    memset(buffer,0,BSIZE);
+   while(1)
+   {
+      connection = accept(sock, (struct sockaddr *) &client_address, &len);
+      char buffer[BSIZE];
+      Command *cmd = malloc(sizeof(Command));
+      State *state = malloc(sizeof(State));
+      pid = fork();
 
-    if(pid<0){
-      fprintf(stderr, "Cannot create child process.");
-      exit(EXIT_FAILURE);
-    }
+      memset(buffer, 0, BSIZE);
 
-    if(pid==0){
-      close(sock);
-      char welcome[BSIZE] = "220 ";
-      if(strlen(welcome_message)<BSIZE-4){
-        strcat(welcome,welcome_message);
-      }else{
-        strcat(welcome, "Welcome to nice FTP service.");
+      if(pid < 0)
+      {
+         fprintf(stderr, "Cannot create child process.");
+         exit(EXIT_FAILURE);
       }
 
-      /* Write welcome message */
-      strcat(welcome,"\n");
-      write(connection, welcome,strlen(welcome));
+      if(pid == 0)
+      {
+         close(sock);
+         char welcome[BSIZE] = "220 ";
 
-      /* Read commands from client */
-      while (bytes_read = read(connection,buffer,BSIZE)){
-        
-        signal(SIGCHLD,my_wait);
+         if(strlen(welcome_message) < BSIZE - 4)
+         {
+            strcat(welcome, welcome_message);
+         }
+         else
+         {
+            strcat(welcome, "Welcome to nice FTP service.");
+         }
 
-        if(!(bytes_read>BSIZE)){
-          /* TODO: output this to log */
-          buffer[BSIZE-1] = '\0';
-          printf("User %s sent command: %s\n",(state->username==0)?"unknown":state->username,buffer);
-          parse_command(buffer,cmd);
-          state->connection = connection;
-          
-          /* Ignore non-ascii char. Ignores telnet command */
-          if(buffer[0]<=127 || buffer[0]>=0){
-            response(cmd,state);
-          }
-          memset(buffer,0,BSIZE);
-          memset(cmd,0,sizeof(cmd));
-        }else{
-          /* Read error */
-          perror("server:read");
-        }
+         /* Write welcome message */
+         strcat(welcome, "\n");
+         write(connection, welcome, strlen(welcome));
+
+         /* Read commands from client */
+         while(bytes_read = read(connection, buffer, BSIZE))
+         {
+
+            signal(SIGCHLD, my_wait);
+
+            if(!(bytes_read > BSIZE))
+            {
+               /* TODO: output this to log */
+               buffer[BSIZE - 1] = '\0';
+               printf("User %s sent command: %s\n", (state->username == 0) ? "unknown" : state->username, buffer);
+               parse_command(buffer, cmd);
+               state->connection = connection;
+
+               /* Ignore non-ascii char. Ignores telnet command */
+               if(buffer[0] <= 127 || buffer[0] >= 0)
+               {
+                  response(cmd, state);
+               }
+
+               memset(buffer, 0, BSIZE);
+               memset(cmd, 0, sizeof(cmd));
+            }
+            else
+            {
+               /* Read error */
+               perror("server:read");
+            }
+         }
+
+         printf("Client disconnected.\n");
+         exit(0);
       }
-      printf("Client disconnected.\n");
-      exit(0);
-    }else{
-      printf("closing... :(\n");
-      close(connection);
-    }
-  }
+      else
+      {
+         printf("closing... :(\n");
+         close(connection);
+      }
+   }
 }
 
 /**
@@ -76,33 +92,39 @@ void server(int port)
  */
 int create_socket(int port)
 {
-  int sock;
-  int reuse = 1;
+   int sock;
+   int reuse = 1;
 
-  /* Server addess */
-  struct sockaddr_in server_address = (struct sockaddr_in){  
-     AF_INET,
-     htons(port),
-     (struct in_addr){INADDR_ANY}
-  };
+   /* Server addess */
+   struct sockaddr_in server_address = (struct sockaddr_in)
+   {
+      AF_INET,
+      htons(port),
+      (struct in_addr)
+      {
+         INADDR_ANY
+      }
+   };
 
 
-  if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    fprintf(stderr, "Cannot open socket");
-    exit(EXIT_FAILURE);
-  }
+   if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+   {
+      fprintf(stderr, "Cannot open socket");
+      exit(EXIT_FAILURE);
+   }
 
-  /* Address can be reused instantly after program exits */
-  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof reuse);
+   /* Address can be reused instantly after program exits */
+   setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof reuse);
 
-  /* Bind socket to server address */
-  if(bind(sock,(struct sockaddr*) &server_address, sizeof(server_address)) < 0){
-    fprintf(stderr, "Cannot bind socket to address");
-    exit(EXIT_FAILURE);
-  }
+   /* Bind socket to server address */
+   if(bind(sock, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
+   {
+      fprintf(stderr, "Cannot bind socket to address");
+      exit(EXIT_FAILURE);
+   }
 
-  listen(sock,5);
-  return sock;
+   listen(sock, 5);
+   return sock;
 }
 
 /**
@@ -112,10 +134,10 @@ int create_socket(int port)
  */
 int accept_connection(int socket)
 {
-  int addrlen = 0;
-  struct sockaddr_in client_address;
-  addrlen = sizeof(client_address);
-  return accept(socket,(struct sockaddr*) &client_address,&addrlen);
+   int addrlen = 0;
+   struct sockaddr_in client_address;
+   addrlen = sizeof(client_address);
+   return accept(socket, (struct sockaddr *) &client_address, &addrlen);
 }
 
 /**
@@ -126,23 +148,24 @@ int accept_connection(int socket)
  */
 void getip(int sock, int *ip)
 {
-  socklen_t addr_size = sizeof(struct sockaddr_in);
-  struct sockaddr_in addr;
-  getsockname(sock, (struct sockaddr *)&addr, &addr_size);
- 
-  char* host = inet_ntoa(addr.sin_addr);
-  sscanf(host,"%d.%d.%d.%d",&ip[0],&ip[1],&ip[2],&ip[3]);
+   socklen_t addr_size = sizeof(struct sockaddr_in);
+   struct sockaddr_in addr;
+   getsockname(sock, (struct sockaddr *)&addr, &addr_size);
+
+   char *host = inet_ntoa(addr.sin_addr);
+   sscanf(host, "%d.%d.%d.%d", &ip[0], &ip[1], &ip[2], &ip[3]);
 }
 
 /**
  * Lookup enum value of string
- * @param cmd Command string 
+ * @param cmd Command string
  * @return Enum index if command found otherwise -1
  */
 
-int lookup_cmd(char *cmd){
-  const int cmdlist_count = sizeof(cmdlist_str)/sizeof(char *);
-  return lookup(cmd, cmdlist_str, cmdlist_count);
+int lookup_cmd(char *cmd)
+{
+   const int cmdlist_count = sizeof(cmdlist_str) / sizeof(char *);
+   return lookup(cmd, cmdlist_str, cmdlist_count);
 }
 
 /**
@@ -155,20 +178,26 @@ int lookup_cmd(char *cmd){
  */
 int lookup(char *needle, const char **haystack, int count)
 {
-  int i;
-  for(i=0;i<count; i++){
-    if(strcmp(needle,haystack[i])==0)return i;
-  }
-  return -1;
+   int i;
+
+   for(i = 0; i < count; i++)
+   {
+      if(strcmp(needle, haystack[i]) == 0)
+      {
+         return i;
+      }
+   }
+
+   return -1;
 }
 
 
-/** 
+/**
  * Writes current state to client
  */
 void write_state(State *state)
 {
-  write(state->connection, state->message, strlen(state->message));
+   write(state->connection, state->message, strlen(state->message));
 }
 
 /**
@@ -177,9 +206,9 @@ void write_state(State *state)
  */
 void gen_port(Port *port)
 {
-  srand(time(NULL));
-  port->p1 = 128 + (rand() % 64);
-  port->p2 = rand() % 0xff;
+   srand(time(NULL));
+   port->p1 = 128 + (rand() % 64);
+   port->p2 = rand() % 0xff;
 
 }
 
@@ -190,7 +219,7 @@ void gen_port(Port *port)
  */
 void parse_command(char *cmdstring, Command *cmd)
 {
-  sscanf(cmdstring,"%s %s",cmd->command,cmd->arg);
+   sscanf(cmdstring, "%s %s", cmd->command, cmd->arg);
 }
 
 /**
@@ -199,13 +228,13 @@ void parse_command(char *cmdstring, Command *cmd)
  */
 void my_wait(int signum)
 {
-  int status;
-  wait(&status);
+   int status;
+   wait(&status);
 }
 
 int main()
 {
-  server(2121);
-  return 0;
+   server(2121);
+   return 0;
 }
 
